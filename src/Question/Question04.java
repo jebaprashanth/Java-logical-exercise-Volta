03efaff012ef49848c409c0deab834adf5607a90
jebaprashanth <100683851+jebaprashanth@users.noreply.github.com>
2023-01-11 09:25:39 +0530

    first commit

--- src/Question/Question04.java
+++ src/Question/Question04.java
@@ -1,10 +1,33 @@
 package Question;
+import java.util.Scanner;
+//
+////4.Write a logic to input characters one by one into a single dimensional array and thereafter reversely
+////print the contents in the array.
+
+
 
 public class Question04 {
+    public static void main(String[] args) {
+    	
+    	
+    	 try (Scanner scanner = new Scanner(System.in)) {
+			char[] input = new char[10];
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			 // Input characters one by one
+			 for (int i = 0; i < 10; i++) {
+			     System.out.print("Enter character " + (i+1) + ": ");
+			     input[i] = scanner.next().charAt(0);
+			 }
+			 
+			 for (int i = 9; i >= 0; i--) {
+			     System.out.print(input[i]);
 
-	}
+			 }
+		}
+         
 
+    }
 }
+
+
+
---
+++ src/Question/Question05.java
@@ -0,0 +1,14 @@
+package Question;
+
+//5.Write a program to check if two given Strings are anagram of each other or not E.g, “Funeral” and
+//“Real Fun” are anagram.
+
+
+public class Question05 {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
